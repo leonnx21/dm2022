91a8d730fe06de1945cfe4a3cd0637afa552e703
leonnx21 <82801582+leonnx21@users.noreply.github.com>
2022-10-25 20:43:12 +0700

    Commit 1
    Finished calculate IDF

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+import re
+import math
+from collections import Counter
+
+data_file = open("/media/leon/MCU/USTH/Data mining/DataMining data/yelp_academic_dataset_review.json")
+D = 0
+textcollection = []
+
+for i, line in enumerate(data_file):
+    x = json.loads(line)['text']
+    x2 = x.lower()
+    x3 = re.sub(r'[^\w\s]', '', x2)
+    x4 = x3.split()
+    textcollection.append(x4)
+    D += 1
+    if i>1000:
+        break
+
+textdict = {}
+
+for i in textcollection:
+    for word in i:
+        if word not in textdict:
+            textdict[word] = 0
+        textdict[word] += 1
+
+dfdict = {}
+
+for key, value in textdict.items():
+    df = int(value)/D
+    idf = math.log(1/df)
+    dfdict[key] = idf
+
+dfdict2 = sorted(dfdict.items(), key=lambda kv: kv[-1])
+dfdict2.reverse()
+
+print(dfdict2)
