2cbe03a83dd49eead8f13ac7653b5982b346e134
leonnx21 <ngxuanloc@gmail.com>
2022-10-27 18:02:01 +0700

    reupload exercise 3

---
+++ 03.review.length.clustering.py
@@ -0,0 +1,72 @@
+import json
+import re
+
+data_file = open("/media/leon/MCU/USTH/Data mining/DataMining data/yelp_academic_dataset_review.json")
+
+n = 9
+
+lengthdict = {}
+for i, line in enumerate(data_file):
+    x = json.loads(line)['text']
+    lengthdict[i] = len(x)
+    if i>n:
+        break
+
+
+def distancematrixcal(lengthdict):
+    distancematrix ={}
+    for k1, v1 in lengthdict.items():
+        distance = {}
+        for k2, v2 in lengthdict.items():
+            distance[k2] = abs(lengthdict[k1] - v2)
+        distancematrix[k1] = distance
+
+    #remove 0
+    for k1, v1 in distancematrix.items():
+        v1 = {key: val for key, val in v1.items() if val != 0}
+        distancematrix[k1] = v1
+    return distancematrix
+
+#print(matrix)
+"""
+for k, v in matrix.items():
+    print(k)
+    print(v)
+"""
+
+def mindistancecal(matrix):
+    distance = []
+    for k1, v1 in matrix.items():
+        k2 = min(v1, key=v1.get)
+        v2 = min(v1.values())
+        distance.append(v2)
+    return min(distance)
+
+def pointstomerge(distancematrix, distance):
+    mindistance = mindistancecal(matrix)
+    for k1, v1 in matrix.items():
+        for k2, v2 in v1.items():
+            if v2 == mindistance:
+                return k1, k2
+
+def mergingpoints(lengthdict, distancematrix):
+    lastposition = len(lengthdict)
+    newvalue = 0
+    for i in pointstomerge(distancematrix, mindistancecal(distancematrix)):
+        newvalue = newvalue + lengthdict[i]
+        lengthdict.pop(i)
+    lengthdict[lastposition] = newvalue/2
+    return lengthdict
+
+print(lengthdict)
+
+for i in range(n-1):
+    matrix = distancematrixcal(lengthdict)
+    lengthdict = mergingpoints(lengthdict, distancematrixcal(lengthdict))
+    print(lengthdict)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import json
 import re
 import math
-from collections import Counter
 
 data_file = open("/media/leon/MCU/USTH/Data mining/DataMining data/yelp_academic_dataset_review.json")
 D = 0
-textcollection = []
 
+textcollection = []
 for i, line in enumerate(data_file):
     x = json.loads(line)['text']
     x2 = x.lower()
@@ -14,29 +13,31 @@ for i, line in enumerate(data_file):
     x4 = x3.split()
     textcollection.append(x4)
     D += 1
-    if i>2000:
+    if i>100000:
         break
 
-textdict = {}
+def dfcount(textcollection):
+    textdict = {}
+    for i in textcollection:
+        for word in i:
+            if word not in textdict:
+                textdict[word] = 1
+        textdict[word] += 1
+    dfdict = {}
 
-for i in textcollection:
-    for word in i:
-        if word not in textdict:
-            textdict[word] = 1
-    textdict[word] += 1
+    for key, value in textdict.items():
+        df = int(value)/D
+        idf = math.log(D/df)
+        dfdict[key] = idf
 
-dfdict = {}
+    dfdict2 = sorted(dfdict.items(), key=lambda kv: kv[1])
+    dfdict2.reverse()
 
-for key, value in textdict.items():
-    df = int(value)/D
-    idf = math.log(1/df)
-    dfdict[key] = idf
+    return dfdict2
 
-dfdict2 = sorted(dfdict.items(), key=lambda kv: kv[1])
-dfdict2.reverse()
 
 fdict ={}
-text = textcollection[1]
+text = textcollection[10]
 
 for word in text:
     if word not in fdict:
@@ -48,11 +49,15 @@ for key, value in fdict.items():
     tf = value/len(text)
     tfdict[key] = tf
 
-
 tf_dfdict ={}
 for k1, v1 in dfdict.items():
     for k2, v2 in tfdict.items():
         if k1 == k2:
             tf_dfdict[k1] = v1*v2
 
-print(tf_dfdict)
+tf_dfdict2 = sorted(tf_dfdict.items(), key=lambda kv: kv[1])
+tf_dfdict2.reverse()
+
+#print(textcollection)
+#print(dfdict2)
+print(tf_dfdict2)
